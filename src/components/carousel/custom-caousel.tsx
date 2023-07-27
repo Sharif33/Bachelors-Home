5b4d4a8dac83cbc4a5b8525893c0aa291488906f
Sharif33 <sharif.cse.nu@gmail.cm>
2023-07-27 21:05:56 +0600

    create slider

---
+++ src/components/carousel/custom-caousel.tsx
@@ -0,0 +1,196 @@
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { Box, useTheme } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import { m } from "framer-motion";
+// import Slider from "@mui/material/Slider";
+import React from "react";
+import { varHover } from "../animate/variants copy";
+
+export interface CarouselItem {
+  image?: string;
+  text?: string;
+}
+
+interface CustomCarouselProps {
+  items: CarouselItem[];
+  loop?: boolean;
+}
+
+const CustomCarousel: React.FC<CustomCarouselProps> = ({
+  items,
+  loop = true,
+}) => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % items.length);
+  };
+
+  const handlePrevious = () => {
+    setActiveIndex(
+      (prevIndex) => (prevIndex - 1 + items.length) % items.length
+    );
+  };
+
+  const handleTabClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  React.useEffect(() => {
+    if (loop) {
+      const interval = setInterval(handleNext, 5000); // Adjust the interval time as needed
+      return () => clearInterval(interval);
+    }
+  }, [activeIndex, loop]);
+
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ position: "relative" }}>
+      {/* <Slider
+        value={activeIndex}
+        onChange={(event, newValue) => setActiveIndex(newValue as number)}
+        step={1}
+        marks
+        min={0}
+        max={items.length - 1}
+        sx={{ display: "none" }}
+      /> */}
+      {items.map((item, index) => (
+        <Box
+          key={index}
+          sx={{
+            display: index === activeIndex ? "block" : "none",
+            width: "100%",
+          }}
+        >
+          {item.image && (
+            <Box
+              component="img"
+              src={item.image}
+              alt={`Slide ${index}`}
+              sx={{
+                width: 1,
+                height: { md: 500, xs: 150 },
+                objectFit: "cover",
+              }}
+            />
+          )}
+          {item.text && <div>{item.text}</div>}
+        </Box>
+      ))}
+      {loop && (
+        <React.Fragment>
+          <IconButton
+            component={m.button}
+            whileTap="tap"
+            whileHover="hover"
+            variants={varHover(1.05)}
+            aria-label="previous"
+            onClick={handlePrevious}
+            sx={{
+              position: "absolute",
+              left: "-20px",
+              top: "50%",
+            }}
+            style={{
+              backgroundColor: theme.palette.primary.main,
+              color: "white",
+            }}
+          >
+            <NavigateBeforeIcon />
+          </IconButton>
+          <IconButton
+            component={m.button}
+            whileTap="tap"
+            whileHover="hover"
+            variants={varHover(1.05)}
+            aria-label="next"
+            onClick={handleNext}
+            sx={{
+              position: "absolute",
+              right: "-20px",
+              top: "50%",
+            }}
+            style={{
+              backgroundColor: theme.palette.primary.main,
+              color: "white",
+            }}
+          >
+            <NavigateNextIcon />
+          </IconButton>
+        </React.Fragment>
+      )}
+      {!loop && (
+        <React.Fragment>
+          <IconButton
+            component={m.button}
+            whileTap="tap"
+            whileHover="hover"
+            variants={varHover(1.05)}
+            aria-label="previous"
+            onClick={handlePrevious}
+            sx={{
+              position: "absolute",
+              left: "-20px",
+              top: "50%",
+            }}
+            // disabled={activeIndex === 0}
+            style={{
+              backgroundColor: theme.palette.primary.main,
+              color: "white",
+            }}
+          >
+            <NavigateBeforeIcon />
+          </IconButton>
+          <IconButton
+            component={m.button}
+            whileTap="tap"
+            whileHover="hover"
+            variants={varHover(1.05)}
+            aria-label="next"
+            onClick={handleNext}
+            sx={{
+              position: "absolute",
+              right: "-20px",
+              top: "50%",
+            }}
+            style={{
+              backgroundColor: theme.palette.primary.main,
+              color: "white",
+            }}
+          >
+            <NavigateNextIcon />
+          </IconButton>
+        </React.Fragment>
+      )}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {items.map((item, index) => (
+          <Box
+            component="img"
+            key={index}
+            src={item.image}
+            alt={`Slide ${index}`}
+            sx={{
+              width: 75,
+              height: 50,
+              mx: 1,
+              cursor: "pointer",
+              opacity: index === activeIndex ? 1 : 0.5,
+              borderRadius: 1,
+            }}
+            onClick={() => handleTabClick(index)}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default CustomCarousel;
--- src/features/houses/single-house.tsx
+++ src/features/houses/single-house.tsx
@@ -1,15 +1,24 @@
+import React from "react";
 import { useParams } from "react-router-dom";
+import CustomCarousel, {
+  CarouselItem,
+} from "../../components/carousel/custom-caousel";
 import { HOUSES } from "../faker/fake-post";
 
 const SingleHouse = () => {
   let { _id } = useParams();
 
   const house = HOUSES?.find((house) => house._id === _id);
-
+  if (!house || !house.images) {
+    return <div>No house found.</div>; // or render a loading state, error message, etc.
+  }
+  const carouselItems: CarouselItem[] = house.images.map((image) => ({
+    image,
+  }));
   return (
-    <div>
-      <p>This is Single House {house?.houseType}</p>
-    </div>
+    <React.Fragment>
+      <CustomCarousel items={carouselItems} loop />
+    </React.Fragment>
   );
 };
 
